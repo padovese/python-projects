d41cad9f7d62215564a56657fe3da0f494c3ef97
padovese <bpadovese@uolinc.com>
2021-01-17 20:38:33 -0300

    feat: increasing wirte and read script

--- hello-python/ex4_write_files.py
+++ hello-python/ex4_write_files.py
@@ -1,13 +1,18 @@
 from sys import argv
+from os.path import exists
+
 script, filename = argv
 
 txt = open(filename, "w")
 
+print "Does the file exist? %r" % exists(filename)
 content =  raw_input("write the content: ")
 txt.write(content)
+txt.close()
 
 print "the result is:"
 
 txt = open(filename)
 print txt.read()
-
+print "The input file is %d bytes long" % len(txt.read())
+txt.close()
