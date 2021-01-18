05719bebc75a7dc505033f9756e089b4286815a0
padovese <bpadovese@uolinc.com>
2021-01-18 10:00:21 -0300

    feat: if and for

---
+++ hello-python/ex8_ifs.py
@@ -0,0 +1,18 @@
+a = 10
+b = 20
+c = 30
+
+if b > a:
+    print "b > a"
+
+if a == b:
+    print "a == b"
+else:
+    print "a != b"
+
+if a == b:
+    print "a == b"
+elif a < b:
+    print "a < b"
+else:
+    print "a != b"
---
+++ hello-python/ex9_lists_and_for_each.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5]
+fruits = ["apples", "bananas", "oranges"]
+
+for number in numbers:
+    print number
+
+fruits.append("watermelon")
+fruits.append("grapes")
+
+for fruit in fruits:
+    print fruit
+
+for i in range(1, 3):
+    print i
