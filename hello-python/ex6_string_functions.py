be882d22ed53d47b0c2ad979ef510033993cffbf
padovese <bpadovese@uolinc.com>
2021-01-17 21:29:08 -0300

    chore: some exercises

---
+++ hello-python/ex6_string_functions.py
@@ -0,0 +1,7 @@
+phrase = "This is a phrase"
+
+splittedAndSorted = sorted(phrase.split(" "))
+
+print splittedAndSorted
+print "first:", splittedAndSorted.pop(0)
+print "last:", splittedAndSorted.pop(-1)
